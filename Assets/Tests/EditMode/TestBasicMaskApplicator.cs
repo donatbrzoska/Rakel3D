1af6e90f19178e9e5199816d681da8e2e0dd3426
Donat Deva Brzoska <donat.brzoska@gmail.com>
2022-07-08 19:13:32 +0200

    redo test design

---
+++ Assets/Tests/EditMode/TestBasicMaskApplicator.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+
+public class TestBasicMaskApplicator
+{
+
+    private Rakel ConstructRakel()
+    {
+        Rakel r = new Rakel(new BasicMaskCalculator(), new BasicMaskApplicator());
+        return r;
+    }
+
+    private void InitializeWithDefaults(Rakel r)
+    {
+        r.UpdateLength(1);
+        r.UpdateWidth(1);
+        r.UpdateColor(new Color(0, 0.4f, 0.8f));
+        r.UpdatePosition(new Vector2Int(1, 1));
+        r.UpdateNormal(Vector2Int.right);
+    }
+
+    [Test]
+    public void Apply_Point()
+    {
+        bool[,] mask = new BasicMaskCalculator().Calculate(1, 1, Vector2.down);
+        Vector2Int maskPosition = new Vector2Int(1, 1);
+        OilPaintTexture t = new OilPaintTexture(3, 3);
+        BasicMaskApplicator bma = new();
+
+        bma.Apply(mask, maskPosition, t, new Color(0, 0.4f, 0.8f));
+
+        Color[] colors = t.GetPixels();
+        AssertUtil.AssertColorsAreEqual(
+            new Color[]
+            {
+                new Color(1, 1, 1), new Color(1, 1, 1),       new Color(1, 1, 1),
+                new Color(1, 1, 1), new Color(0, 0.4f, 0.8f), new Color(1, 1, 1),
+                new Color(1, 1, 1), new Color(1, 1, 1),       new Color(1, 1, 1),
+            },
+            colors
+        );
+    }
+
+    [Test]
+    public void Apply_Rectangle()
+    {
+        bool[,] mask = new BasicMaskCalculator().Calculate(3, 2, Vector2.right);
+        Vector2Int maskPosition = new Vector2Int(2, 1);
+        OilPaintTexture t = new OilPaintTexture(3, 3);
+        BasicMaskApplicator bma = new();
+
+        bma.Apply(mask, maskPosition, t, new Color(0, 0.4f, 0.8f));
+
+        Color[] colors = t.GetPixels();
+        AssertUtil.AssertColorsAreEqual(
+            new Color[]
+            {
+                new Color(1, 1, 1), new Color(0, 0.4f, 0.8f), new Color(0, 0.4f, 0.8f),
+                new Color(1, 1, 1), new Color(0, 0.4f, 0.8f), new Color(0, 0.4f, 0.8f),
+                new Color(1, 1, 1), new Color(0, 0.4f, 0.8f), new Color(0, 0.4f, 0.8f),
+            },
+            colors
+        );
+    }
+
+    [Test]
+    public void Apply_Rectangle_Rotated90_SymmetricCase()
+    {
+        bool[,] mask = new BasicMaskCalculator().Calculate(3, 2, Vector2.down);
+        Vector2Int maskPosition = new Vector2Int(1, 1);
+        OilPaintTexture t = new OilPaintTexture(3, 3);
+        BasicMaskApplicator bma = new();
+
+        bma.Apply(mask, maskPosition, t, new Color(0, 0.4f, 0.8f));
+
+        Color[] colors = t.GetPixels();
+        //LogUtil.Log(colors, 3);
+        AssertUtil.AssertColorsAreEqual(
+            // NOTE the written array representation is the real coordinate system but mirrored by the x axis!
+            new Color[]
+            {
+                new Color(1, 1, 1),       new Color(1, 1, 1),       new Color(1, 1, 1),
+                new Color(0, 0.4f, 0.8f), new Color(0, 0.4f, 0.8f), new Color(0, 0.4f, 0.8f),
+                new Color(0, 0.4f, 0.8f), new Color(0, 0.4f, 0.8f), new Color(0, 0.4f, 0.8f),
+            },
+            colors
+        );
+    }
+
+    [Test]
+    public void Apply_Rectangle_Rotated90_LargerTextureAsymmetricCase()
+    {
+        bool[,] mask = new BasicMaskCalculator().Calculate(3, 2, Vector2.down);
+        Vector2Int maskPosition = new Vector2Int(1, 4);
+        OilPaintTexture t = new OilPaintTexture(3, 6);
+        BasicMaskApplicator bma = new();
+
+        bma.Apply(mask, maskPosition, t, new Color(0, 0.4f, 0.8f));
+
+        Color[] colors = t.GetPixels();
+        //LogUtil.Log(colors, 6);
+        AssertUtil.AssertColorsAreEqual(
+            // NOTE the written array representation is the real coordinate system but mirrored by the x axis!
+            new Color[]
+            {
+                new Color(1, 1, 1),       new Color(1, 1, 1),       new Color(1, 1, 1),
+                new Color(1, 1, 1),       new Color(1, 1, 1),       new Color(1, 1, 1),
+                new Color(1, 1, 1),       new Color(1, 1, 1),       new Color(1, 1, 1),
+                new Color(1, 1, 1),       new Color(1, 1, 1),       new Color(1, 1, 1),
+                new Color(0, 0.4f, 0.8f), new Color(0, 0.4f, 0.8f), new Color(0, 0.4f, 0.8f),
+                new Color(0, 0.4f, 0.8f), new Color(0, 0.4f, 0.8f), new Color(0, 0.4f, 0.8f),
+            },
+            colors
+        );
+    }
+}
---
+++ Assets/Tests/EditMode/TestRakel_ReapplyMask.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+
+class BasicMaskApplicatorMock : BasicMaskApplicator
+{
+    public string Log { get; private set; }
+    public override void Apply(bool[,] mask, Vector2Int maskPosition, OilPaintTexture texture, Color color)
+    {
+        Log += "Apply ";
+    }
+}
+
+public class TestRakel_ReapplyMask
+{
+    OilPaintTexture texture;
+    Rakel rakel;
+    BasicMaskApplicatorMock ma_mock;
+
+    [SetUp]
+    public void Init()
+    {
+        texture = new OilPaintTexture(3, 3);
+        ma_mock = new BasicMaskApplicatorMock();
+        rakel = new Rakel(new BasicMaskCalculator(), ma_mock);
+        rakel.UpdateLength(1);
+        rakel.UpdateWidth(1);
+        rakel.UpdateColor(new Color(0, 0.4f, 0.8f));
+        rakel.UpdatePosition(new Vector2Int(1, 1));
+        rakel.UpdateNormal(Vector2Int.right);
+    }
+
+    [Test]
+    public void ApplyToCanvas_ReapplyMask_UpdateWidth()
+    {
+        // initial value set
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply ", ma_mock.Log);
+
+        // nothing changed -> no reapply
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply ", ma_mock.Log);
+
+        // make change -> recalculate
+        rakel.UpdateWidth(2);
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply Apply ", ma_mock.Log);
+
+        // nothing changed -> no reapply
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply Apply ", ma_mock.Log);
+    }
+
+    [Test]
+    public void ApplyToCanvas_ReapplyMask_UpdateLength()
+    {
+        // initial value set
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply ", ma_mock.Log);
+
+        // nothing changed -> no reapply
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply ", ma_mock.Log);
+
+        // make change -> recalculate
+        rakel.UpdateLength(2);
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply Apply ", ma_mock.Log);
+
+        // nothing changed -> no reapply
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply Apply ", ma_mock.Log);
+    }
+
+    [Test]
+    public void ApplyToCanvas_ReapplyMask_UpdateNormal()
+    {
+        // initial value set
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply ", ma_mock.Log);
+
+        // nothing changed -> no reapply
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply ", ma_mock.Log);
+
+        // make change -> recalculate
+        rakel.UpdateNormal(new Vector2(0, 0));
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply Apply ", ma_mock.Log);
+
+        // nothing changed -> no reapply
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply Apply ", ma_mock.Log);
+    }
+
+    [Test]
+    public void ApplyToCanvas_ReapplyMask_UpdatePosition()
+    {
+        // initial value set
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply ", ma_mock.Log);
+
+        // nothing changed -> no reapply
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply ", ma_mock.Log);
+
+        // make change -> recalculate
+        rakel.UpdatePosition(new Vector2Int(0, 0));
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply Apply ", ma_mock.Log);
+
+        // nothing changed -> no reapply
+        rakel.ApplyToCanvas(texture);
+        Assert.AreEqual("Apply Apply ", ma_mock.Log);
+    }
+}
